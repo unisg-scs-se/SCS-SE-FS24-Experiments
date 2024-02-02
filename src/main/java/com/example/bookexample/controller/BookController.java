b96c9cdaf9587f10b7f9f8af309d29c9c590c958
marco <marco.franceschetti@unisg.ch>
2024-02-02 17:07:51 +0100

    Added comment to BookController.

--- src/main/java/com/example/bookexample/controller/BookController.java
+++ src/main/java/com/example/bookexample/controller/BookController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 //A small change to the project - to be removed later
+//agree
 @RestController
 public class BookController {
 
